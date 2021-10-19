4df7e67d576c21c0c1939d3543cabe92bd979f9e
unknown <rmanjakasoajoseph@gmail.com>
2021-10-20 00:04:20 +0200

    post in homepage

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -1,29 +1,34 @@
-import React,{ useEffect } from 'react';
+import React,{ useEffect,useState } from 'react';
 import axios from 'axios';
 
 const HomePage = (props) => {
+	const [posts,setPosts] = useState([]);
+	const [currentPage,setCurrentPage] = useState(1); 
 	useEffect(() => {
-		axios.get('http://localhost:8000/api/posts')
-		.then(response => console.log(response));
-	},[])
+		axios.get(`http://localhost:8000/api/posts?page=${currentPage}`)
+		 .then(response => response.data['hydra:member'])
+		 .then(data => setPosts(data));
+	},[currentPage])
+	const handleClick = () => {
+		setCurrentPage(currentPage+1)
+	}
 	return (
-		<div className="list-group">
-		  <a href="#" className="list-group-item list-group-item-action flex-column align-items-start active">
-		    <div className="d-flex w-100 justify-content-between">
-		      <h5 className="mb-1">List group item heading</h5>
-		      <small>3 days ago</small>
-		    </div>
-		    <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-		    <small>Donec id elit non mi porta.</small>
-		  </a>
-		  <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
-		    <div className="d-flex w-100 justify-content-between">
-		      <h5 className="mb-1">List group item heading</h5>
-		      <small className="text-muted">3 days ago</small>
-		    </div>
-		    <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-		    <small className="text-muted">Donec id elit non mi porta.</small>
-		  </a>
+		<div>
+			{posts.map(post =>(
+				<div key={post.id} className="list-group mt-5">
+				  <a href="#" className="list-group-item list-group-item-action flex-column align-items-start active">
+				    <div className="d-flex w-100 justify-content-between">
+				      <h5 className="mb-1">{post.fullNameUser}</h5>
+				      <small>3 days ago</small>
+				    </div>
+				  </a>
+				  <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+				   
+				    {post.content}
+				  </a>
+				</div>
+			))}
+			<button className="btn btn-primary" onClick={handleClick} >Plus d'actualité</button>
 		</div>
 	);
 }
